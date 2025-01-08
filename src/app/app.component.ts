d9a90e5a295433081d61590e446b7d50dd46fe9b
Himanshu Mehra <himanshumehra@Himanshus-MacBook-Air.local>
2025-01-08 23:21:48 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import axios from 'axios';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,22 @@ import { HeaderComponent } from './header/header.component';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit() {
+    this.getBoards();
+  }
   title = 'trello';
+
+ async getBoards(){
+  const { trelloApiKey, trelloAccessToken } = environment;
+  const url = `https://api.trello.com/1/members/me/boards?key=${trelloApiKey}&token=${trelloAccessToken}`;
+
+    try{
+      const response = await axios.get(url);
+      console.log(response.data);
+      
+    }catch(error){
+      console.error('Error fetching boards:', error);
+    }
+ }
 }
